ce412181cf84617e0b3a6791eea1ebe189ca0fa3
Nikita Rawat <54388754+rawatnikita@users.noreply.github.com>
2022-02-17 22:39:53 +0530

    Add files via upload
    Refactoring changes

--- code/drive.java
+++ code/drive.java
@@ -8,8 +8,9 @@ public class drive {
 		int numLanes = 3;
 		int maxPatronsPerParty=5;
 
-		Alley a = new Alley( numLanes );
-		ControlDesk controlDesk = a.getControlDesk();
+		// Alley a = new Alley( numLanes );
+		ControlDesk controlDesk = new ControlDesk( numLanes ) ;
+		// a.getControlDesk();
 
 		ControlDeskView cdv = new ControlDeskView( controlDesk, maxPatronsPerParty);
 		controlDesk.subscribe( cdv );
