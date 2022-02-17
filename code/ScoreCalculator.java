8e92e72372c34bc78363a3a4112c57e85925ab79
Debashish <debashish.roy1998@gmail.com>
2022-02-17 23:15:52 +0530

    Reduced Cyclomatic Complexity

--- code/ScoreCalculator.java
+++ code/ScoreCalculator.java
@@ -18,7 +18,6 @@ public class ScoreCalculator {
                 cumulScores[i][j] = 0;
             }
         }
-
     }
 
     public int getFinalScore() {
@@ -46,46 +45,47 @@ public class ScoreCalculator {
         return maxSum;
     }
 
-    public int getScore(int frame, int ball, int[] curScore) {
+    public void calculateCummulativeScore(int i, int curScore[], int current) {
+
+        if (i == 1 && i < current - 1) {
+            cumulScores[bowlIndex][i / 2] -= curScore[i + 1] / 2;
+        } else {
+            int maxSum = calculateMaxSum(cumulScores, (i / 2) - 1, bowlIndex);
+            cumulScores[bowlIndex][i / 2] -= maxSum / 2;
+        }
+    }
+
+    public boolean checkStrike(int i, int curScore[]) {
+        if (curScore[i + 2] != -1 && (curScore[i + 3] != -1 || curScore[i + 4] != -1)) {
+            strike(curScore, i);
+            return false;
+        }
+        return true;
+    }
+
+    public int getScore(int frame, int ball, int[] curScore) {      //remove return type,remove total score
         int totalScore = 0;
-        for (int i = 0; i != 10; i++) {
+        for (int i = 0; i < 10; i++) {
             cumulScores[bowlIndex][i] = 0;
         }
+
         int current = 2 * (frame - 1) + ball - 1;
         // Iterate through each ball until the current one.
 
-        for (int i = 0; i != current + 2; i++) {
+        for (int i = 0; i < current + 2; i++) {
             int now_score = curScore[i];
             if (i % 2 == 1 && now_score == -2 && curScore[i - 1] == -2 && i < 19) {
-
-                if (i == 1 && i < (current - 1)) {
-                    double score = (0.5) * (curScore[i + 1]);
-                    cumulScores[bowlIndex][(i / 2)] -= (int) score;
-                } else {
-
-                    int cell = (i / 2) - 1;
-
-                    int maxSum = calculateMaxSum(cumulScores, cell, bowlIndex);
-
-                    double score1 = (0.5) * (maxSum);
-                    cumulScores[bowlIndex][(i / 2)] -= (int) score1;
-
-                }
+                calculateCummulativeScore(i, curScore, current);
             } else if (i % 2 == 1 && now_score + curScore[i - 1] == 10 && i < (current - 1) && i < 19) {
 
                 // This ball was a the second of a spare.
                 // Also, we're not on the current ball.
                 // Add the next ball to the ith one in cumul.
                 cumulScores[bowlIndex][(i / 2)] += curScore[i + 1] + now_score;
-            } else if (i < current && i % 2 == 0 && now_score == 10 && i < 18) {
+            } else if (i < current && i % 2 == 0 && now_score == 10 && i < 18 && checkStrike(i, curScore)) {
                 // This ball is the first ball, and was a strike.
                 // If we can get 2 balls after it, good add them to cumul.
-                if (curScore[i + 2] != -1 && (curScore[i + 3] != -1 || curScore[i + 4] != -1)) {
-                    // Ok, got it.
-                    strike(curScore, i);
-                } else {
-                    break;
-                }
+                break;
             } else {
                 // We're dealing with a normal throw, add it and be on our way.
                 normal(curScore, i);
@@ -96,22 +96,18 @@ public class ScoreCalculator {
     }
 
     private void normal(int[] curScore, int i) {
-        if (i < 18) {
-            if (i % 2 == 0) {
-                if (i == 0) {
-                    cumulScores[bowlIndex][i / 2] += (curScore[i] != -2) ? curScore[i] : 0;
-                } else {
-                    // add his last frame's cumul to this ball, make it this frame's cumul.
-                    int add = (curScore[i] != -2) ? curScore[i] : 0;
-                    cumulScores[bowlIndex][i / 2] += cumulScores[bowlIndex][i / 2 - 1] + add;
-                }
-            } else if (curScore[i] != -1) {
-                int add = (curScore[i] != -2) ? curScore[i] : 0;
-                cumulScores[bowlIndex][i / 2] += add;
+        int add = (curScore[i] != -2) ? curScore[i] : 0;
 
+        if (i < 18) {
+            if (i % 2 == 0 && i != 0) {
+                // add his last frame's cumul to this ball, make it this frame's cumul.
+                add += cumulScores[bowlIndex][i / 2 - 1];
+            } else if (curScore[i] != -1) {
+                cumulScores[bowlIndex][i / 2] += add;
             }
-        } else if ((i / 2 == 9 || i / 2 == 10)) {
-            int add = (curScore[i] != -2) ? curScore[i] : 0;
+            cumulScores[bowlIndex][i / 2] += add;
+        } else if (i < 22) {
+
             cumulScores[bowlIndex][9] += add;
         }
         if (i == 18) {
--- code/drive.java
+++ code/drive.java
@@ -1,5 +1,3 @@
-import java.util.Vector;
-import java.io.*;
 
 public class drive {
 
@@ -8,8 +6,7 @@ public class drive {
 		int numLanes = 3;
 		int maxPatronsPerParty=5;
 
-		Alley a = new Alley( numLanes );
-		ControlDesk controlDesk = a.getControlDesk();
+		ControlDesk controlDesk = new ControlDesk( numLanes ) ;
 
 		ControlDeskView cdv = new ControlDeskView( controlDesk, maxPatronsPerParty);
 		controlDesk.subscribe( cdv );
